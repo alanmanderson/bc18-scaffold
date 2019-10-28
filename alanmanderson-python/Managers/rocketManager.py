f9a0586c7a978966e0d2a46f3154654ce7668fdc
Alan Anderson <alan.anderson@udacity.com>
2019-10-28 15:38:08 -0700

    Add rocketManager

---
+++ alanmanderson-python/Managers/rocketManager.py
@@ -0,0 +1,12 @@
+import battlecode as bc
+
+class RocketManager():
+  def __init__(self, controller):
+    self.controller = controller
+
+  def handle(self, unit):
+    pass
+#    print(self.controller.starting_map(self.controller.planet().other()).to_json())
+#    if len(unit.structure_garrison()) == unit.structure_max_capacity():
+#      if self.controller.can_launch(unit.id, ):
+
